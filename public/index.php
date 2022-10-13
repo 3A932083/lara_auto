e64e0479c7eae5bc729987c04b99d7c8d280b3de
jaing shu yi <3a932083@gm.student.ncut.edu.tw>
2022-10-14 00:24:41 +0800

    renew index.php

--- public/index.php
+++ public/index.php
@@ -19,7 +19,7 @@ use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
 
 // create a log channel
-$log = new Logger('name');
+$log = new Logger('name');echo '<br>';
 $log->pushHandler(new StreamHandler('../log/my.log', Level::Warning));
 
 // add records to the log
@@ -29,8 +29,8 @@ $log->error('Bar');
 //Carbon
 use Carbon\Carbon;
 
-printf("Right now is %s", Carbon::now()->toDateTimeString());
-printf("Right now in Vancouver is %s", Carbon::now('America/Vancouver'));  //implicit __toString()
+//printf("現在的時間是 %s", Carbon::now()->toDateTimeString());echo '<br>';
+printf("現在台灣台北的時間是 %s", Carbon::now('Asia/Taipei'));  //implicit __toString()
 $tomorrow = Carbon::now()->addDay();
 $lastWeek = Carbon::now()->subWeek();echo '<br>';
 $nextSummerOlympics = Carbon::createFromDate(2016)->addYears(4);
@@ -38,7 +38,7 @@ $nextSummerOlympics = Carbon::createFromDate(2016)->addYears(4);
 $officialDate = Carbon::now()->toRfc2822String();
 $howOldAmI = Carbon::createFromDate(1975, 5, 21)->age;
 
-$noonTodayLondonTime = Carbon::createFromTime(12, 0, 0, 'Europe/London');
+$noonTodayLondonTime = Carbon::createFromTime(12, 0, 0, 'Asia/Taipei');
 
 $internetWillBlowUpOn = Carbon::create(2038, 01, 19, 3, 14, 7, 'GMT');
 
@@ -57,13 +57,13 @@ if (Carbon::now()->isWeekend()) {
     echo 'Party!';
 }
 // Over 200 languages (and over 500 regional variants) supported:
-echo Carbon::now()->subMinutes(2)->diffForHumans();// '2 minutes ago'
-echo'<br>';
-echo Carbon::now()->subMinutes(2)->locale('zh_CN')->diffForHumans(); // '2分钟前'
-echo'<br>';
-echo Carbon::parse('2019-07-23 14:51')->isoFormat('LLLL'); // 'Tuesday, July 23, 2019 2:51 PM'
-echo'<br>';
-echo Carbon::parse('2019-07-23 14:51')->locale('fr_FR')->isoFormat('LLLL'); // 'mardi 23 juillet 2019 14:51'
+//echo Carbon::now()->subMinutes(2)->diffForHumans();// '2 minutes ago'
+//echo'<br>';
+//echo Carbon::now()->subMinutes(2)->locale('zh_CN')->diffForHumans(); // '2分钟前'
+
+
+//echo Carbon::parse('2019-07-23 14:51')->isoFormat('LLLL');
+//echo Carbon::parse('2019-07-23 14:51')->locale('fr_FR')->isoFormat('LLLL'); // 'mardi 23 juillet 2019 14:51'
 
 // ... but also does 'from now', 'after' and 'before'
 // rolling up to seconds, minutes, hours, days, months, years
